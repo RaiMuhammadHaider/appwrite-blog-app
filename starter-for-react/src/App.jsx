33b9683b6835a72534c8cf7962d739648d65262f
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-09-05 01:36:27 -0700

    first commit

---
+++ starter-for-react/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+
+function App() {
+
+  
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold bg-amber-200">ali</h1>
+    </>
+   
+  );
+}
+
+export default App;
