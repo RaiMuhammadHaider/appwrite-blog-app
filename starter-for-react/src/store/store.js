27cc458b99c4f1ea9f52bfc898d9d8455b891315
Rai Muhammad Haider <raimuhammadalihaider@gmail.com>
2025-09-09 06:36:00 -0700

    Store

---
+++ starter-for-react/src/store/store.js
@@ -0,0 +1,19 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice.js';
+ const store = configureStore({
+    reducer: {
+        auth: authSlice.reducer,
+    },
+})
+export default store;
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import authReducer from "./authSlice.js";
+
+// const store = configureStore({
+//     reducer: {
+//         auth: authReducer,
+//     },
+// });
+
+// export default store;
